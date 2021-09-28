b316a0a995ef6d2e87442563ff028385202d7596
jupiterlander <20988990+jupiterlander@users.noreply.github.com>
2021-09-28 08:42:43 +0200

    Removed unused vars

--- src/utils/socketConnection.js
+++ src/utils/socketConnection.js
@@ -2,6 +2,6 @@ import socketIOClient from "socket.io-client";
 const ENDPOINT = "https://jsramverk-editor-adpr12.azurewebsites.net/";
 const LOCAL = 'http://localhost:1337';
 
-const socket = socketIOClient(LOCAL);
+const socket = socketIOClient(process.env.NODE_ENV === 'production'? ENDPOINT: LOCAL);
 
 export default socket;
