5cebd10780fb469bbf91d8878ff806921b5607df
jupiterlander <20988990+jupiterlander@users.noreply.github.com>
2021-09-28 10:30:39 +0200

    Bugfix: correct url

--- src/utils/socketConnection.js
+++ src/utils/socketConnection.js
@@ -1,7 +1,7 @@
 import socketIOClient from "socket.io-client";
 const ENDPOINT = "https://jsramverk-editor-adpr12.azurewebsites.net/";
-const LOCAL = 'http://localhost:1337';
+//const LOCAL = 'http://localhost:1337';
 
-const socket = socketIOClient(process.env.NODE_ENV === 'production'? ENDPOINT: LOCAL);
+const socket = socketIOClient(ENDPOINT);
 
 export default socket;
